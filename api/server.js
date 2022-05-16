07971e3ea31985f5b7fa3734a571d61152ea6914
devantequick <devantequick94@gmail.com>
2022-05-16 15:46:44 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const accountRouter = require("./accounts/accounts-router");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accountRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
 
 module.exports = server;
